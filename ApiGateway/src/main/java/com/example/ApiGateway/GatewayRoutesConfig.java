05a69e21a84f731a02b6330a5fe4d295c62619bc
Muhammad-Muzamil <Muhammad-Muzamil1>
2026-04-14 22:09:13 +0500

    Docker File added into each service and Docker Compose file is added inside root directory of the Project Folder

--- ApiGateway/src/main/java/com/example/ApiGateway/GatewayRoutesConfig.java
+++ ApiGateway/src/main/java/com/example/ApiGateway/GatewayRoutesConfig.java
@@ -2,14 +2,9 @@ package com.example.ApiGateway;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
-import java.util.Arrays;
-import java.util.List;
 
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
